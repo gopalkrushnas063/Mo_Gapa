54f9e8151a6ae27a3f1bbfa5dcf0eb0381ff294b
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-11-20 10:14:16 +0530

    in story details section animation effect added

--- lib/pages/story_details_view.dart
+++ lib/pages/story_details_view.dart
@@ -5,6 +5,8 @@ import 'package:share/share.dart';
 import 'package:ui/model/story.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
+
 
 
 
@@ -107,8 +109,9 @@ class StoryDetailsPage extends StatelessWidget {
                   ),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
-                    child: Image.network(
-                      imageUrl,
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: imageUrl,
                       width: double.infinity,
                       height: 350,
                       fit: BoxFit.cover,
