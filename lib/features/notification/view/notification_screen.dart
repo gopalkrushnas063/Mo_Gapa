37ea11e64c04fedca23e1ed6feed00583260b5ca
GOPAL KRUSHNA SAHOO <103574856+gopalkrushnas063@users.noreply.github.com>
2024-08-15 23:20:40 +0530

    notification screen updated

--- lib/features/notification/view/notification_screen.dart
+++ lib/features/notification/view/notification_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mogapabahi/data/model/story.dart';
+import 'package:mogapabahi/features/introduction/view/introduction_page.dart';
 import 'package:mogapabahi/features/notification/controller/notification_provider.dart';
+import 'package:mogapabahi/features/story/view/story_details_view.dart';
 import 'package:mogapabahi/utility/enum.dart';
 
 class NotificationScreen extends ConsumerStatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _NotificationScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _NotificationScreenState();
 }
 
 class _NotificationScreenState extends ConsumerState<NotificationScreen> {
@@ -17,7 +20,9 @@ class _NotificationScreenState extends ConsumerState<NotificationScreen> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback(
       (_) async {
-        await ref.read(notificationsProvider.notifier).fetchAndDisplayNotifications();
+        await ref
+            .read(notificationsProvider.notifier)
+            .fetchAndDisplayNotifications();
       },
     );
   }
@@ -26,27 +31,96 @@ class _NotificationScreenState extends ConsumerState<NotificationScreen> {
   Widget build(BuildContext context) {
     final notificationState = ref.watch(notificationsProvider);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notifications'),
+    return WillPopScope(
+      onWillPop: () async {
+        // Navigate back to ContentScreen instead of exiting the app
+        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  IntroductionPage(),
+                            ),
+                          );
+        return false; // Prevent default back button behavior
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notifications'),
+        ),
+        body: notificationState.status == APIState.loading
+            ? const Center(child: CircularProgressIndicator())
+            : notificationState.status == APIState.failed
+                ? const Center(child: Text('Failed to load notifications'))
+                : ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: notificationState.stories?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      StoryModel story = notificationState.stories![index];
+                      return ListTile(
+                        title: Card(
+                          elevation: 4.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          margin: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    story.image,
+                                    width: 100,
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                const SizedBox(width: 16.0),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        story.title,
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 8.0),
+                                      Text(
+                                        story.content,
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          color: Colors.grey[700],
+                                        ),
+                                        maxLines: 2,
+                                        overflow: TextOverflow
+                                            .ellipsis, // Add ellipsis
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  StoryDetailsPage(story: story),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
       ),
-      body: notificationState.status == APIState.loading
-          ? const Center(child: CircularProgressIndicator())
-          : notificationState.status == APIState.failed
-              ? const Center(child: Text('Failed to load notifications'))
-              : ListView.builder(
-                  itemCount: notificationState.stories?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    StoryModel story = notificationState.stories![index];
-                    return ListTile(
-                      title: Text(story.title ?? 'No Title'),
-                      subtitle: Text(story.content ?? 'No Subtitle'),
-                      onTap: () {
-                        // Navigate to a detailed screen with the story information
-                      },
-                    );
-                  },
-                ),
     );
   }
 }
